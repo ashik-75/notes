ee4aa6bbdda280f290c713014d5f315bfe0dfad0
Ashik <ashik@pop-os.localdomain>
2023-06-01 20:40:43 +0600

    add logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,19 @@
+import { Scan } from "lucide-react";
 import { Link } from "react-router-dom";
 import MobileMenu from "./MobileMenu";
 import SignIn from "./SignIn";
 
 function Header() {
   return (
-    <div className="fixed left-0 top-0 z-50 w-full border-b border-zinc-700 bg-white/10 p-5 backdrop-blur-md">
+    <div className="fixed left-0 top-0 z-50 w-full border-b border-zinc-800 bg-white/10 p-5 backdrop-blur-md">
       <div className="mx-auto flex max-w-7xl items-center justify-between">
-        <div className="font-poppins text-lg font-bold text-zinc-100">
-          <Link to={"/"}>CLAY</Link>
+        <div className="font-poppins font-roboto text-lg text-zinc-100">
+          <Link to={"/"}>
+            <Scan className="h-10 w-10 text-white" />
+          </Link>
         </div>
 
-        <ul className="hidden space-x-6 text-xs tracking-[4px] text-zinc-500 sm:flex">
+        <ul className="hidden space-x-6 text-xs tracking-[4px] text-zinc-400 sm:flex">
           <li>
             <Link to={"/stories"}>STORIES</Link>
           </li>
